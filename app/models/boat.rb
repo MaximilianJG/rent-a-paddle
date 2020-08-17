66d2c73683ea124a0b959884ed1c7c95f95a4c7c
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-17 17:54:09 +0200

    added validations to all models, created index  boats

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,9 @@
 class Boat < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :bookings
+
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :boat
+
+  validates :starting_date_time, presence: true
+  validates :ending_date_time, presence: true
 end
