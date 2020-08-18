b139503155a5c973dcac0348218a9b1aeaf82f1e
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-18 22:33:56 +0200

    Cards are roughly done

--- app/policies/boat_policy.rb
+++ app/policies/boat_policy.rb
@@ -1,10 +1,12 @@
 class BoatPolicy < ApplicationPolicy
-  class Scope < Scope
+
+ class Scope < Scope
     def resolve
       scope.all
     end
   end
 
+    # We don't need this because we have got 'except index' in the applciation controller
     def index?
       return true
     end
@@ -13,16 +15,20 @@ class BoatPolicy < ApplicationPolicy
       return true
     end
 
+    # 'def new?' is in application_policy.rb
     def create?
       return true
+    end
+
+    # 'def edit?' is in application_policy.rb
+    def update?
+      record.user == user || user.admin
     end
 
     def destroy?
       record.user == user || user.admin
     end
 
-    def update?
-      record.user == user || user.admin
-    end
+
 
 end
