d74829abaec4570a8564bd7f042e1d8100d987bf
Tim Jenner <timothy.jenner.biz@gmail.com>
2020-08-18 14:00:02 +0200

    Created new and create in boat controller, gave authorization

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,7 +1,35 @@
 class BoatsController < ApplicationController
 
+
   def index
     @boats = Boat.all
   end
 
+  def new
+    @boat = Boat.new
+    authorize @boat
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    authorize @boat
+    if @boat.save
+      redirect_to boats_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+
+
+end
+
+private
+
+def boat_params
+  params.require(:boat).permit(:name, :type, :price, :location, :description)
 end
