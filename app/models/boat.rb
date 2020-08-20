ae0a5535e534610169a102521ffeeac9449f04e6
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-20 12:33:15 +0200

    User can upload one picture now, form requires you to upload picture, could implement an if/else statement to check if photo is present

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,7 @@
 class Boat < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :bookings
+  has_one_attached :photo
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location? # do I change this to location?
@@ -10,4 +11,5 @@ class Boat < ApplicationRecord
   # # validates :type, presence: true
   # validates :price, presence: true
   # validates :location, presence: true
+  validates :photo, presence: true
 end
